6f09b482ab4b842b3eedbb620b07fa4f3d25afb4
adrian <adrianribeiroprofissional.com>
2026-05-12 11:44:03 -0300

    fazendo a area de exebição

--- src/zone/massas/showDataMassas.js
+++ src/zone/massas/showDataMassas.js
@@ -3,10 +3,21 @@ import { db } from "../../utils/config/firebase.js"
 
 
 export async function getDataMassas(id) {
+    let show = document.querySelector("#show")
+    show.innerHTML = ""
 
     const q = query(collection(db, "receitas"), or(where("select", "==", id), where("favorito", "==", id)))
     const data = await getDocs(q)
-    data.forEach((doc) => {
-        console.log(doc.id, doc.data())
-    })
+    const receitas = data.docs.map(item => ({
+        id: item.id,
+        ...item.data()
+    }))
+
+    for(let i in receitas){
+        const h1 = document.createElement("h1")
+        h1.innerHTML = receitas[i].name
+
+        show.appendChild(h1)
+
+    }
 }
